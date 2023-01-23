b2f0a1614a20be119dee3b765d86644bff94d105
lashaade <gnomi490@mail.com>
2023-01-23 20:35:35 +0400

    script two

---
+++ script.js
@@ -0,0 +1,14 @@
+const country = `georgia`;
+const continet= `europe`;
+const language= `georgian`;
+const populationOfCountry= 3800000;
+console.log(populationOfCountry /2);
+console.log (populationOfCountry>6000000);
+console.log(`this is ${country} is is located on the continet of ${continet} ,they speak ${language}`);
+if (populationOfCountry > 5000000){
+    console.log( `population is more 5000000`)
+}if (populationOfCountry <3000000){
+    console.log(`population is less 3000000`)
+} else {
+    console.log(`population is between 3000000-5000000`)
+}
